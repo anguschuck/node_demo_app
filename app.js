ecd56232c83024cb5e0a243a0372306edefd49d9
Christian Roth <christianroth@macbook-pro-von-christian.home>
2023-05-12 16:21:36 +0200

    Initial Setup

---
+++ app.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./db");
+const port = 3000;
+
+// Connect to database
+connectDB();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Your app is listening on port ${port}`);
+});
