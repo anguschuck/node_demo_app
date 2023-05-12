0434b3dc22beaab7c9d42a1df97db715a046cd09
anguschuck <anguschuck@hotmail.com>
2023-05-12 19:44:43 +0200

    URI direct

--- db.js
+++ db.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
-const db = process.env.DB_URI;
+//const db = process.env.DB_URI;
 
-//const db = 'mongodb+srv://anguschuck:rpdm5fezs3Z5y8uL@clusterdemoapp.ye7fw9q.mongodb.net/?retryWrites=true&w=majority';
+const db = 'mongodb+srv://anguschuck:rpdm5fezs3Z5y8uL@clusterdemoapp.ye7fw9q.mongodb.net/?retryWrites=true&w=majority';
+
+console.log(db);
 
 async function connectDB() {
     try {
